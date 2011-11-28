c0063dd83eead16954cfbc2cc3284c773a69bea0
byplayer <byplayer100@gmail.com>
2011-11-28 16:37:48 +0900

    initial commit

---
+++ test.rb
@@ -0,0 +1,29 @@
+require 'bundler/setup'
+require 'roma/client'
+
+servers = [
+           'localhost:12000',
+           'localhost:12001'
+          ]
+
+client = Roma::Client::RomaClient.new(servers)
+
+puts "# set []"
+puts client['test'] = 'test value'
+
+puts "# get []"
+puts client['test']
+
+puts "# delete"
+puts client.delete('test')
+
+puts "# set method with expire"
+puts client.set('test2', 'test2 value', '2')
+
+puts "# get method"
+puts client['test2']
+
+sleep(5)
+puts "# get method2"
+puts client['test2']
+
