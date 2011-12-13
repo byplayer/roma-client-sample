68a897d90361b3c0f7b1456c7fdaa9a38e6f9364
yukio.goto <yukio.goto@mail.rakuten.com>
2011-12-13 17:31:53 +0900

    add sample_alist

---
+++ sample_alist.rb
@@ -0,0 +1,21 @@
+require 'roma-client'
+require 'roma-client-plugin-alist'
+
+puts "RomaClient version: #{Roma::Client::VERSION::STRING}"
+puts "alist version     : #{Roma::Client::Plugin::AList::VERSION::STRING}"
+
+servers = [
+           'localhost:12000',
+           'localhost:12001'
+          ]
+
+
+pool = Roma::Client::ClientPool.instance(:test)
+pool.servers = servers
+pool.add_plugin_module(Roma::Client::Plugin::AList)
+client = pool.client
+
+p client.included_modules
+
+puts "alist_empty?: #{client.alist_empty?('test')}"
+
